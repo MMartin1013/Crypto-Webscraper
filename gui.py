1eacb10d318db8740c377938ecd7d1b5743136f8
Manolo Martin <martinmano38@gmail.com>
2022-01-08 18:03:16 -0500

    Gui works with webscraper

--- gui.py
+++ gui.py
@@ -1,20 +1,73 @@
 from tkinter import *
-from typing import Collection
-from PIL import ImageTk, Image
-import WebScraper
+from webscraper import WebScraper
 
+#Defines webscraper
+scraper = WebScraper()
+
+#Initializes and defines dimensions for root window
 root = Tk()
 root.title('Crypto Stats')
-root.geometry('600x600')
-title = Label(root, text="Crypto Info Gatherer").grid(row=0, column=1)
-frame = LabelFrame(root, text="Your Mother", pady=200,padx=200, bg='white')
-frame.grid(pady=50,padx=50, column=0,row=1, sticky='N', columnspan=3)
-exit_button = Button(root, text='Exit', command=root.quit).grid(row=2, column=0)
-info_button = Button(root, text='Info', command='').grid(row=2, column= 1)
-holder_button = Button(root, text='Ino', command='').grid(row=2, column= 2)
+root.geometry('700x750')
+root.minsize(700,750)
+
+#Initializes frame to hold data
+frame = LabelFrame(root, text="Info", bg='white', fg ='black', padx=25,pady=25)
+frame.place(relx=0.5, rely=0.4, anchor=CENTER)
+placeholder = Label(frame, text='', bg='white').pack(padx=200, pady=200)
 
 
-label = Label(frame, text="Your Moth").pack()
+#Clears the frame
+def clear():
+    widgets = frame.pack_slaves()
+    for widget in widgets:
+        widget.destroy()
 
+#Displays trending data
+def trending():
+    clear()
+    trendinginfo = scraper.get_categories()
+    for category in trendinginfo:
+        Label(frame, text=category.name + ':', bg='white', fg='black', font='Helvetica 24 bold').pack(anchor='w')
+        for company in category.companies:
+            formatframe = LabelFrame(frame, text='', bg='white', fg ='black', borderwidth=0, highlightthickness=0)
+            formatframe.pack()
+            Label(formatframe,text=company.rank, bg='white', fg='black',font='Helvetica 16 bold').pack(side='left')
+            Label(formatframe,text=company.name, bg='white', fg='black',font='Helvetica 16').pack(side='left')
+            if company.pricechange[0] == '-':
+                color = 'red' 
+            elif company.pricechange[0] == '+':
+                color = 'green'
+            else:
+                color = 'black'
+            Label(formatframe,text=company.pricechange, bg='white', fg=color,font='Helvetica 16').pack(side='right')
+
+
+    
+#Displays top5 companies
+def topfive():
+    clear()
+    top5 = scraper.get_table()
+    for company in top5:
+        Label(frame,text=company.rank + ' ' + company.name + ':', bg='white', fg='black',font='Helvetica 12 bold').pack(anchor='w')
+        formatframe = LabelFrame(frame, text='', bg='white', fg ='black', borderwidth=0, highlightthickness=0)
+        formatframe.pack()
+        Label(formatframe,text=company.get_pricechange(), bg='white', fg='black', font='Helvetica 12').pack(side='left')
+        color = 'red' if company.dayprice[0] == '-' else 'green'
+        Label(formatframe,text=company.get_dayprice(), bg='white', fg=color, font='Helvetica 12').pack(side='left')
+        color = 'red' if company.sevendayprice[0] == '-' else 'green'
+        Label(formatframe,text=company.get_sevendayprice(), bg='white', fg=color,font='Helvetica 12').pack(side='left')
+        Label(formatframe,text=company.get_marketcap(), bg='white', fg='black',font='Helvetica 12').pack(side='left')
+        Label(formatframe,text=company.get_volume(), bg='white', fg='black',font='Helvetica 12').pack(side='left')
+        Label(frame,text=company.get_circulatingsupply(), bg='white', fg='black',font='Helvetica 12').pack()
+    
+    
+
+
+#Title and buttons
+title = Label(root, text="Crypto Info Gatherer").pack(anchor=CENTER)
+exit_button = Button(root, text='Exit', command=root.quit).place(relx=0.25, rely=0.8, anchor=CENTER)
+trending_button = Button(root, text='Trending', command=lambda: trending()).place(relx=0.5, rely=0.8, anchor=CENTER)
+top5_button = Button(root, text='Top 5', command=lambda: topfive()).place(relx=0.75, rely=0.8, anchor=CENTER)
+clear_button = Button(root, text='Clear', command=lambda: clear()).place(relx=0.5, rely=0.90, anchor=CENTER)
 
 root.mainloop()
--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+#This program gathers and stores information from the coin market cap website to be displayed in the gui
 
+
+#Class to hold company information
 class Company:
     def __init__(self):
         self.name = None
@@ -20,7 +23,7 @@ class Company:
         return 'Rank: ' + self.rank
     
     def get_pricechange(self):
-        return 'PriceChange: ' + self.pricechange
+        return 'Price: ' + self.pricechange
 
     def get_dayprice(self):
         return '24H%: ' + self.dayprice if self.dayprice != None else ''
@@ -29,13 +32,13 @@ class Company:
         return '7D%: ' + self.sevendayprice if self.sevendayprice != None else ''
     
     def get_marketcap(self):
-        return 'MarketCap: ' + self.marketcap if self.marketcap != None else ''
+        return 'Market Cap: ' + self.marketcap if self.marketcap != None else ''
 
     def get_volume(self):
         return 'Volume: ' + self.volume if self.volume != None else ''
 
     def get_circulatingsupply(self):
-        return 'CirculatingSupply: ' + self.circulatingsupply if self.circulatingsupply != None else ''   
+        return 'Circulating Supply: ' + self.circulatingsupply if self.circulatingsupply != None else ''   
         
     def get_img(self):
         return 'Img: ' + self.img if self.img != None else ''
@@ -46,7 +49,7 @@ class Company:
     def __str__(self):
         return '{} {} {} {} {} {} {} {} {}'.format(self.get_name(),self.get_rank(),self.get_pricechange(),self.get_dayprice(),self.get_sevendayprice(),self.get_marketcap(),self.get_volume(),self.get_circulatingsupply(),self.get_img()).strip()
 
-
+#Class to hold category information
 class Category:
     
     def __init__(self):
@@ -54,7 +57,7 @@ class Category:
         self.companies = []
     
     def get_name(self):
-        return self.name
+        return self.name + '\n'
 
     def get_companies(self):
         return self.companies    
@@ -77,58 +80,73 @@ class WebScraper:
 
     #Returns an
     def get_categories(self):  
+        self.page = urlopen('https://coinmarketcap.com')
+        self.soup = BeautifulSoup(self.page, 'lxml')
+        #Return value
         info = []
         #Obtains tending companies
         categories = self.soup.find_all(class_='sc-16r8icm-0 sc-1uagfi2-0 bdEGog sc-1rmt1nr-1 eCWTbV')
         for category in categories: 
-            #Obtains name of category and array of companies for that category
+            #New object to hold category information
             newCategory = Category()
+            
+            #Obtains name of category and array of companies for that category
             newCategory.name = category.find(class_='sc-1rmt1nr-0 sc-1rmt1nr-3 jCyFIz').div.get_text()
             companies = category.find_all(class_='sc-1rmt1nr-4')
             
-            #print(f'{newCategory.name}: ')
+            #Gets information for each company in the category
             for company in companies:
+                #New object to hold company information
                 newCompany = Company()
-                #Obcures trending companies name and rank
+                
+                #Obcures trending companies name and rank and stores them in newcomapny class
                 newCompany.name = company.find(class_='sc-16r8icm-0').get_text()
                 newCompany.rank = company.find(class_='rank').get_text()
             
                 #Polarity used to calculate whether the price is up or down
                 polarity = company.find(class_='icon-Caret-up')
                 #Stores price change based on polarity if the company hasn't been recently added
+                
                 #Stores neutral price otherwise
-                if(newCompany.name != 'Recently Added'):
+                if(newCategory.name != 'Recently Added'):
                     newCompany.pricechange = '-' + company.find(class_='price-change').get_text() if (polarity == None) else  '+' + company.find(class_='price-change').get_text()
                 else:
                     newCompany.pricechange = company.find(class_='price-change').get_text()
 
+                #Adds new company to the list of companies in new Category
                 newCategory.companies.append(newCompany)
                 
-            #print('\n')
+            #Adds new category to the table
             info.append(newCategory)
         return info
 
 #Table of cryptocurrencies
 
+    #Gets info from the first 10 elements on the coin market cap table
     def get_table(self):
+        self.page = urlopen('https://coinmarketcap.com')
+        self.soup = BeautifulSoup(self.page, 'lxml')
+        #Return value
         info = []
         #Obtains table and its rows
         table = self.soup.find(class_='h7vnx2-2 czTsgW cmc-table').find('tbody')
         rows = table.find_all('tr')
 
-        #Gets the first 10 items
-        for i in range(10):
+        #Gets the first 5 items
+        for i in range(5):
+            #New instance of company class to store company information
             newCompany = Company()
-            #Obtains their statistics
+            
+            #Obtains their statistics and stores them into new company object
             newCompany.rank = rows[i].find(class_='sc-1eb5slv-0 etpvrL').get_text()
             newCompany.name = rows[i].find(class_='sc-16r8icm-0 sc-1teo54s-1 dNOTPP').find('p').get_text()
             newCompany.pricechange = rows[i].find(class_='sc-131di3y-0 cLgOOr').get_text()
             prices = rows[i].find_all(class_='sc-15yy2pl-0')
             dayprice = prices[0]
             sevendayprice = prices[1]
-            newCompany.marketCap = rows[i].find(class_='sc-1ow4cwt-1 ieFnWP').get_text()
+            newCompany.marketcap = rows[i].find(class_='sc-1ow4cwt-1 ieFnWP').get_text()
             newCompany.volume = rows[i].find(class_='sc-1eb5slv-0 hykWbK font_weight_500').get_text()
-            newCompany.circulatingSupply = rows[i].find(class_= 'sc-1eb5slv-0 kZlTnE').get_text()
+            newCompany.circulatingsupply = rows[i].find(class_= 'sc-1eb5slv-0 kZlTnE').get_text()
             newCompany.image = rows[i].find('img').get('src')
 
             #24H prices and 7d Prices have a chance to not exist
@@ -140,9 +158,9 @@ class WebScraper:
                 polarity = sevendayprice.find(class_='icon-Caret-up')
                 newCompany.sevendayprice = '-' + sevendayprice.get_text() if (polarity == None) else  '+' + sevendayprice.get_text()
 
+            #Adds new company to list
             info.append(newCompany)
 
-            #print(f'Rank: {rank} Name: {name} Price: {price} 24Hr%: {dayprice} 7d%: {sevendayprice} MarketCap: {marketCap} Volume: {volume} CirculatingSupply: {circulatingSupply} Image: {image}')
         return info
 
     
